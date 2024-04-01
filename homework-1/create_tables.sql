e8cf56dbfdda72387527ff05098154237eba1d8a
savva <savva.net@icloud.com>
2024-04-01 11:37:13 +1000

    Solution

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees_data (
+	employee_id int PRIMARY KEY,
+	first_name varchar(20),
+	last_name varchar(20),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers_data (
+	customer_id varchar(20) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+CREATE TABLE orders_data (
+	order_id int PRIMARY KEY,
+	customer_id varchar (20) REFERENCES customers_data(customer_id),
+	employee_id int REFERENCES employees_data(employee_id),
+	order_data date,
+	ship_city varchar(100)
+);
+
+SELECT * FROM customers_data
+
+SELECT * FROM employees_data
+
+SELECT * FROM orders_data
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="Touch12345"
+)
+
+cur = conn.cursor()
+
+with open('north_data/customers_data.csv', 'r', encoding="utf-8") as f:
+    next(f)
+    cur.copy_from(f, 'customers_data', sep=',')
+    conn.commit()
+
+with open('north_data/employees_data.csv', 'r', newline="", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cur.execute("INSERT INTO employees_data VALUES (%s, %s, %s, %s, %s, %s)", row)
+    conn.commit()
+
+with open('north_data/orders_data.csv', 'r', newline="", encoding="utf-8") as f:
+    next(f)
+    cur.copy_from(f, 'orders_data', sep=',')
+    conn.commit()
